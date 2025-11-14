bb5d9db9aff05895d1305898601a10f207cedede
Brixton <auvray.cedric00@gmail.com>
2025-11-14 14:48:54 +0100

    vercel urls updated

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'my-moviz-backend-mauve.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'my-moviz-frontend-five-ivory.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
